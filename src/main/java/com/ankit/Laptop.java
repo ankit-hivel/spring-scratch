c5e1915986461ff54ebf9fe9eda10101ab62c92c
Ankit Ydv <ankityadav11241@gmail.com>
2026-02-20 11:46:12 +0530

    object refs

---
+++ src/main/java/com/ankit/Laptop.java
@@ -0,0 +1,17 @@
+package com.ankit;
+
+public class Laptop {
+    private String name;
+
+    public void compile() {
+        System.out.println("compiling...");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/ankit/Programmer.java
+++ src/main/java/com/ankit/Programmer.java
@@ -2,11 +2,30 @@ package com.ankit;
 
 public class Programmer {
     private int age;
+    private Laptop laptop;
+
     public Programmer() {
         System.out.println("Programmer class constructor");
     }
 
     public void code(){
         System.out.println("Coding..." + age);
+        laptop.compile();
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
