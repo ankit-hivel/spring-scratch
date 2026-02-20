19b68504899e4fb75d31f00a1748944f37efd2a0
Ankit Ydv <ankityadav11241@gmail.com>
2026-02-20 11:38:25 +0530

    spring scratch setup

---
+++ src/main/java/com/ankit/App.java
@@ -0,0 +1,22 @@
+package com.ankit;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println( "Hello World!" );
+        Programmer pg1 = (Programmer) ctx.getBean("programmer");
+        pg1.code();
+
+        Programmer pg2 = (Programmer) ctx.getBean("programmer");
+        pg2.code();
+    }
+}
---
+++ src/main/java/com/ankit/Programmer.java
@@ -0,0 +1,12 @@
+package com.ankit;
+
+public class Programmer {
+    private int age;
+    public Programmer() {
+        System.out.println("Programmer class constructor");
+    }
+
+    public void code(){
+        System.out.println("Coding..." + age);
+    }
+}
